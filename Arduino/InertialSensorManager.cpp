912cec4ca1e2ab3a9b88f9a5fbbddcfbeeab631c
Andrew Wintenberg <awintenb@gmail.com>
2014-12-13 01:11:07 -0500

    Added Arduino subdirectory

---
+++ Arduino/InertialSensorManager.cpp
@@ -0,0 +1,21 @@
+// 
+// 
+// 
+
+#include "InertialSensorManager.h"
+
+Orientation eulerOr;
+
+void init_inertial_sensors(void) {
+	init_MinIMU9();
+}
+
+void update_inertial_sensors(void) {
+	update_MinIMU9();
+	eulerOr.pitch = getPitch();
+	eulerOr.yaw = getYaw();
+	eulerOr.roll = getRoll();
+}
+
+
+
---
+++ Arduino/message.h
@@ -0,0 +1,38 @@
+#ifndef COMMUNICATE_MESSAGE_H_
+#define COMMUNICATE_MESSAGE_H_
+
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "Arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+#define MAX_MESSAGE_LENGTH 32
+#define BAUD_RATE 115200
+#define MEMBER_SIZE(type, member) sizeof(((type *)0)->member)
+
+enum message_state_t {
+  WAITING_FOR_LENGTH,
+  WAITING_FOR_ACTION,
+  WAITING_FOR_MESSAGE,
+  MESSAGE_READY
+};
+
+struct message_data_t {
+  struct {
+    byte length;
+    byte action;
+  } header;
+  byte body[MAX_MESSAGE_LENGTH];
+};
+
+struct message_t {
+  enum message_state_t state;
+  struct message_data_t data;
+};
+
+void init_messages(void);
+void update_messages(void);
+
+#endif // COMMUNICATE_MESSAGE_H_
